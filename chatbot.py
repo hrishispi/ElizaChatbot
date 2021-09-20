a4f50c9cd65eb4a0aca5f15958b75a921e124267
spark1201 <90984570+spark1201@users.noreply.github.com>
2021-09-20 01:39:10 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,12 +4,14 @@ Created on Wed Sep 15 05:44:06 2021
 Class: AIT 526
 @author: Prakriti Panday, Sean Park, Alice Chen , Hrishikesh Karambelkar
 """
+# import all the necessary packages
 import datetime
 import random
 import re
 
 from nltk.tokenize import word_tokenize
 
+# model dictionary where the key words are defined as well as the responses.
 model = {
 
     r'.*(kids?|child(ren)?).*':
@@ -111,6 +113,7 @@ model = {
 
 }
 
+# define pronoun to transform 2nd pov to 1st pov and 1st pov to 2nd pov, vice versa.
 pronoun = {
     "you": "me",
     "yours": "mine",
@@ -127,71 +130,90 @@ pronoun = {
 
 }
 
-
+# tokenize the matching words and iterate lists of tokenizer
 def replace_pronouns(match):
     tokenizer = word_tokenize(match)
-
+    
+    # call the pronoun definition and replace the element in tokenizer, join the tokens, and return tokenizer
     for x, token in enumerate(tokenizer):
         if token in pronoun:
             tokenizer[x] = pronoun[token]
     return ' '.join(tokenizer)
 
-
+# define conversation input and return answer
 def conversation_input(user_input):
+    # iterate over the "conversation_dict"
     for pattern, responses in model.items():
-
+        # checks for matching input vs the key
         ans = re.search(pattern, user_input.rstrip(",.!"))
-
+        # if the key matches the input, returns (prints) the "response"
         if ans:
+            # the response will be randomly returned out of all the given "conversation_dict" under its key word
             response = random.choice(responses)
+            # if the question in "conversation_dict" includes "%s," the response will return with the information from the input
             if "%s" in response:
                 match = ans[1]
+                # when and if the key word include 2nd or 1st pov ("you" or "me"), it will transform to 1st or 2nd pov vice versa.
                 response = response % (replace_pronouns(match))
                 return response
             else:
                 return response
 
-
+# check input user name and return a greeting message, "Eliza: Welcome %s. What would you like to share with me today?" with the user name replaced in %s
 def process(user_name):
     print("Eliza: Welcome %s. What would you like to share with me today?" % user_name)
+    # standard expression for bye
     end = [r'.*bye.*', r'.*exit.*']
     user_continue = True
     while user_continue:
+        #check and get input name and information 
         user_input = input('%s: ' % user_name)
         print()
+        # convert input into lowercase
         user_input = user_input.lower()
 
+        # check for "end" input such as 'bye' or 'exit'
         for i in end:
             user_exit = re.findall(i, user_input)
+            # if there is an "end" input, exit the while loop  
             if len(user_exit) != 0:
                 user_continue = False
+        # if there is no "end" input, continue and return an appropriate response 
         if user_continue:
             response = conversation_input(user_input)
             print("Eliza: " + response)
 
-
+# extra functionality to greet with a proper time of the day: morning, afternoon, and evening
 def greet():
     currentTime = datetime.datetime.now()
+    # if user time zone is before 12:00pm, greet with "Good morning."
     if currentTime.hour < 12:
         return "Good morning."
+    # if user time zone is or after 12:00pm and before 6:00pm , greet with "Good afternoon."
     elif 12 <= currentTime.hour < 18:
         return "Good afternoon."
+    # otherwise greet with "Good evening."
     else:
         return "Good evening."
 
-
+# initialize dialogue and return message with user name input along with extra functionality datetime greet
 def initiate_conversation():
     print("Eliza: %s I'm Eliza, a psychotherapist. Who do I have the pleasure of speaking to today?" % greet())
+    # initialize user name input
     name_input = input('User: ')
     print()
+    # tokenize user name input
     tokenizer = word_tokenize(name_input)
+    # use the last word as user name if name
     user_name = tokenizer[-1]
     return user_name
 
-
+# main function to initialize chat conversation
 def main():
     user_name = initiate_conversation()
+    # main process (conversation)
     process(user_name)
+    # end message
     print("Eliza: Goodbye ")
 
 
